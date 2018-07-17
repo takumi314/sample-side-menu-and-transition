48af14ddd0d493b469da7626d344178defa6d7f9
takumi314 <isospin_symmetry@me.com>
2018-07-17 23:24:21 +0900

    Initial init

---
+++ sample-side-menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample-side-menu
+//
+//  Created by NishiokaKohei on 2018/07/11.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
