23e24a1ea907fe0b8b25719fdf360fa52d611d35
takumi314 <isospin_symmetry@me.com>
2018-07-19 00:47:02 +0900

    add initializer

--- sample-side-menu/MainViewController.swift
+++ sample-side-menu/MainViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+
+    // MARK: - Private
+
+    private var contentViewController: UIViewController
+    private var sidemeneViewController: SideMenuViewController
+
+    ///
+    /// return true if SideMenu is presenteted on self.
+    ///
+    private var isShownSidemenu: Bool {
+        return sidemeneViewController.parent == self
+    }
+
+    // MARK: - Initializer
+
+    init(_ child: SideMenuViewController) {
+        self.contentViewController = UIViewController()
+        self.sidemeneViewController = child
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        self.contentViewController = UIViewController()
+        self.sidemeneViewController = SideMenuViewController()
+        super.init(coder: aDecoder)
+    }
+
+
+    // MARK: - life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
