43b6b1c13784886f95bc8d3cf2739706b4750a65
takumi314 <isospin_symmetry@me.com>
2018-07-23 00:48:06 +0900

    [feat] add new ViewController

---
+++ sample-side-menu/SideMenuBaseController.swift
@@ -0,0 +1,35 @@
+//
+//  SideMenuBaseController.swift
+//  sample-side-menu
+//
+//  Created by NishiokaKohei on 2018/07/23.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuBaseController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
